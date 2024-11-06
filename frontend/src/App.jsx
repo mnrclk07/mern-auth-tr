dfa096fdda702733ea8944f599efdec94309ab34
mnrclk07 <mnrclk21@gmail.com>
2024-11-06 03:55:14 +0300

    create pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,30 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Project from "./pages/Project";
+import Dashboard from "./pages/Dashboard";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import Search from "./pages/Search";
+import EmailVerification from "./pages/EmailVerification";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/project" element={<Project />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/verify-email" element={<EmailVerification />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
+  );
 }
