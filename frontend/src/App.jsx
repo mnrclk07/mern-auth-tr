d39e605fbe847e632f34ff3c579b634d0a960d6e
mnrclk07 <mnrclk21@gmail.com>
2024-11-06 20:06:49 +0300

    create the header section and projects page name change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Project from "./pages/Project";
+import Projects from "./pages/Projects";
 import Dashboard from "./pages/Dashboard";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
@@ -10,18 +10,75 @@ import Search from "./pages/Search";
 import EmailVerification from "./pages/EmailVerification";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
+import Header from "./pages/Components/Header";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/project" element={<Project />} />
-      <Route path="/search" element={<Search />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/register" element={<Register />} />
+      <Route
+        path="/"
+        element={
+          <div>
+            <Header />
+            <Home />
+          </div>
+        }
+      />
+      <Route
+        path="/about"
+        element={
+          <div>
+            <Header />
+            <About />
+          </div>
+        }
+      />
+      <Route
+        path="/projects"
+        element={
+          <div>
+            <Header />
+            <Projects />
+          </div>
+        }
+      />
+      <Route
+        path="/search"
+        element={
+          <div>
+            <Header />
+            <Search />
+          </div>
+        }
+      />
+      <Route
+        path="/dashboard"
+        element={
+          <div>
+            <Header />
+            <Dashboard />
+          </div>
+        }
+      />
+      <Route
+        path="/register"
+        element={
+          <div>
+            <Header />
+            <Register />
+          </div>
+        }
+      />
       <Route path="/verify-email" element={<EmailVerification />} />
-      <Route path="/login" element={<Login />} />
+      <Route
+        path="/login"
+        element={
+          <div>
+            <Header />
+            <Login />
+          </div>
+        }
+      />
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/reset-password" element={<ResetPassword />} />
       <Route path="/*" element={<NotFound />} />
